17a7ec8b8e7b4bff74fe9dce69b1244589b22e28
Evgenii Petrov <blinunleius@gmail.com>
2025-06-15 17:31:13 +0100

    test: use insta snapshot tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,9 +1,8 @@
-use assert_cmd::prelude::*;
-use assert_fs::prelude::*;
-use predicates::prelude::*;
-use std::process::Command;
+use assert_fs::prelude::FileWriteStr;
 
-// TODO: switch to https://docs.rs/trycmd/latest/trycmd or similar
+fn cli() -> Result<assert_cmd::Command, Box<dyn std::error::Error>> {
+    Ok(assert_cmd::Command::cargo_bin("logseq-srs")?)
+}
 
 #[test]
 fn single_top_level_card() -> Result<(), Box<dyn std::error::Error>> {
@@ -21,21 +20,12 @@ fn single_top_level_card() -> Result<(), Box<dyn std::error::Error>> {
 - Not card
 "#;
     file.write_str(content)?;
-    let expected_output = r#"- What is a sphere? #card
-  card-last-interval:: 244.14
-  card-repeats:: 6
-  card-ease-factor:: 3.1
-  card-next-schedule:: 2025-11-21T00:00:00.000Z
-  card-last-reviewed:: 2025-03-22T09:54:57.202Z
-  card-last-score:: 5
-  - Set of points in a 3 dimensional space that are equidistant from a center point.
-"#;
-    let mut cmd = Command::cargo_bin("logseq-srs")?;
+
+    let mut cmd = cli()?;
 
     cmd.arg("cards-in-file").arg(file.path());
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::diff(expected_output));
+    let output = String::from_utf8(cmd.output().unwrap().stdout).unwrap();
+    insta::assert_snapshot!(output);
 
     Ok(())
 }
@@ -58,23 +48,12 @@ fn card_with_data_after_metadata() -> Result<(), Box<dyn std::error::Error>> {
 - Not card
 "#;
     file.write_str(content)?;
-    let expected_output = r#"- What is the relationship between angles $\\alpha$ and $\\gamma_{1}$ in the picture relative to the transversal?
-  card-last-interval:: 30.0
-  card-repeats:: 6
-  card-ease-factor:: 2.5
-  card-next-schedule:: 2026-01-01T00:00:00.000Z
-  card-last-reviewed:: 2025-12-02T00:00:00.000Z
-  card-last-score:: 5
-  https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Transverzala_8.svg/262px-Transverzala_8.svg.png
-  #card
-  - They are alternate angles.
-"#;
-    let mut cmd = Command::cargo_bin("logseq-srs")?;
+
+    let mut cmd = cli()?;
 
     cmd.arg("cards-in-file").arg(file.path());
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::diff(expected_output));
+    let output = String::from_utf8(cmd.output().unwrap().stdout).unwrap();
+    insta::assert_snapshot!(output);
 
     Ok(())
 }
@@ -97,23 +76,12 @@ fn card_with_unicode_prompt() -> Result<(), Box<dyn std::error::Error>> {
 - Not card
 "#;
     file.write_str(content)?;
-    let expected_output = r#"- Какова связь между углами $\\alpha$ и $\\gamma_{1}$ на изображении относительно секущей?
-  card-last-interval:: 30.0
-  card-repeats:: 6
-  card-ease-factor:: 2.5
-  card-next-schedule:: 2026-01-01T00:00:00.000Z
-  card-last-reviewed:: 2025-12-02T00:00:00.000Z
-  card-last-score:: 5
-  https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Transverzala_8.svg/262px-Transverzala_8.svg.png
-  #card
-  - Они накрест лежащие.
-"#;
-    let mut cmd = Command::cargo_bin("logseq-srs")?;
+
+    let mut cmd = cli()?;
 
     cmd.arg("cards-in-file").arg(file.path());
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::diff(expected_output));
+    let output = String::from_utf8(cmd.output().unwrap().stdout).unwrap();
+    insta::assert_snapshot!(output);
 
     Ok(())
 }
@@ -134,16 +102,21 @@ fn single_top_level_card_metadata() -> Result<(), Box<dyn std::error::Error>> {
 - Not card
 "#;
     file.write_str(content)?;
-    // TODO: elide the filepath prefix, can't do an exact match otherwise
-    //let expected_output = r#"CardMetadata { source_path: "/tmp/.tmpHHjPRD/page.md", prompt_fingerprint: 724424550506611259, prompt_prefix: "- What is a sphere? #card" }"#;
-    let mut cmd = Command::cargo_bin("logseq-srs")?;
+    let mut cmd = cli()?;
 
     cmd.arg("cards-in-file")
         .arg("--output=metadata")
         .arg(file.path());
-    cmd.assert().success().stdout(predicate::str::contains(
-        "prompt_fingerprint : 219dda4ed3b53642",
-    ));
+    let output = String::from_utf8(cmd.output().unwrap().stdout).unwrap();
+
+    insta::with_settings!({
+        filters => vec![
+            (r"/tmp/.tmp\w+/", "[TMP_DIR]/"),
+        ],
+    },
+    {
+        insta::assert_snapshot!(output);
+    });
 
     Ok(())
 }
