8e095f10f377b130fb04cfd0d1f6fe0c75d354e6
Evgenii Petrov <blinunleius@gmail.com>
2025-06-22 12:01:28 +0100

    test: add page lines for insta snapshots

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,25 +1,69 @@
-use assert_fs::prelude::FileWriteStr;
-use insta_cmd::{assert_cmd_snapshot, get_cargo_bin};
+use std::path::Path;
 use std::process::Command;
 
-fn cli() -> Command {
-    Command::new(get_cargo_bin("logseq-srs"))
+use serde::Serialize;
+
+use assert_fs::prelude::FileWriteStr;
+use insta_cmd::get_cargo_bin;
+
+#[derive(Serialize)]
+pub struct Info {
+    program: String,
+    args: Vec<String>,
+    page_lines: Vec<String>,
+}
+
+impl Info {
+    fn new(cmd: &Command, page: &str) -> Self {
+        Info {
+            program: insta_cmd_describe_program(cmd.get_program()),
+            args: cmd.get_args().map(|x| x.to_string_lossy().into_owned()).collect(),
+            page_lines: page.split("\n").map(|s| s.to_owned()).collect(),
+        }
+    }
+}
+
+// Extracted from macros
+// https://github.com/mitsuhiko/insta-cmd/blob/0.6.0/src/macros.rs#L11-L17
+fn insta_cmd_format_output(output: std::process::Output) -> String {
+    format!(
+        "success: {:?}\nexit_code: {}\n----- stdout -----\n{}\n----- stderr -----\n{}",
+        output.status.success(),
+        output.status.code().unwrap_or(!0),
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr),
+    )
+}
+
+// Extracted from private function
+// https://github.com/mitsuhiko/insta-cmd/blob/0.6.0/src/spawn.rs#L22-L30
+fn insta_cmd_describe_program(cmd: &std::ffi::OsStr) -> String {
+    let filename = Path::new(cmd).file_name().unwrap();
+    let name = filename.to_string_lossy();
+    let name = &name as &str;
+    name.into()
 }
 
 macro_rules! test_card_output {
-    ($name:ident, $cmd:expr, $args:expr, $content:expr ) => {
+    ($name:ident, $subcommand:expr, $args:expr, $page:expr ) => {
         #[test]
         fn $name() {
             let file = assert_fs::NamedTempFile::new("page.md").unwrap();
-            file.write_str($content).unwrap();
+            file.write_str($page).unwrap();
+
+            let mut cmd = Command::new(get_cargo_bin("logseq-srs"));
+            cmd.arg($subcommand).arg(file.path()).args($args);
+            let output = cmd.output().unwrap();
 
             insta::with_settings!({
+                omit_expression => true,
+                info => &Info::new(&cmd, $page),
                 filters => vec![
                     (r"/tmp/.tmp\w+/", "[TMP_DIR]/"),
                 ],
             },
             {
-                assert_cmd_snapshot!(cli().arg($cmd).arg(file.path()).args($args));
+                insta::assert_snapshot!(insta_cmd_format_output(output));
             });
         }
     };
