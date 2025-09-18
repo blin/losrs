42a96de2adcd9f069b66f6690ea37f7c7e78dc79
Evgenii Petrov <blinunleius@gmail.com>
2025-09-18 09:28:18 +0100

    feat: split review timestamp into updating and selection (#18)

--- src/review.rs
+++ src/review.rs
@@ -1,6 +1,7 @@
 use anyhow::Context;
 use anyhow::Ok;
 use anyhow::Result;
+use anyhow::anyhow;
 use chrono::DateTime;
 use chrono::FixedOffset;
 use rs_fsrs::FSRS;
@@ -30,34 +31,50 @@ impl From<&ReviewResponse> for Rating {
     }
 }
 
-fn compute_next_fsrs_meta(
-    srs_meta: &SRSMeta,
-    resp: &ReviewResponse,
+struct ReviewableFSRSMeta<'a> {
+    inner: &'a FSRSMeta,
     reviewed_at: DateTime<FixedOffset>,
-) -> FSRSMeta {
+}
+
+impl<'a> ReviewableFSRSMeta<'a> {
+    fn new(fsrs_meta: &'a FSRSMeta, reviewed_at: DateTime<FixedOffset>) -> Result<Self> {
+        if reviewed_at < fsrs_meta.last_review.fixed_offset() {
+            return Err(anyhow!(
+                "reviewing a card that was last reviewed on {:?} at {:?}, before it was last reviewed!",
+                fsrs_meta.last_review,
+                reviewed_at
+            ));
+        };
+        Ok(Self { inner: fsrs_meta, reviewed_at })
+    }
+}
+
+fn compute_next_fsrs_meta(fsrs_meta: &ReviewableFSRSMeta, resp: &ReviewResponse) -> FSRSMeta {
+    let reviewed_at = fsrs_meta.reviewed_at;
     let fsrs_params = rs_fsrs::Parameters { enable_short_term: false, ..Default::default() };
     let fsrs = FSRS::new(fsrs_params);
 
-    let next = fsrs.next(srs_meta.fsrs_meta.clone(), reviewed_at.into(), resp.into());
+    let next = fsrs.next(fsrs_meta.inner.clone(), reviewed_at.into(), resp.into());
     next.card
 }
 
-fn compute_next_srs_meta(
-    srs_meta: &SRSMeta,
-    resp: &ReviewResponse,
-    reviewed_at: DateTime<FixedOffset>,
-) -> SRSMeta {
-    let next_fsrs_meta = compute_next_fsrs_meta(srs_meta, resp, reviewed_at);
+fn compute_next_srs_meta(fsrs_meta: &ReviewableFSRSMeta, resp: &ReviewResponse) -> SRSMeta {
+    let next_fsrs_meta = compute_next_fsrs_meta(fsrs_meta, resp);
     let next_logseq_srs_meta = (&next_fsrs_meta).into();
 
     SRSMeta { logseq_srs_meta: next_logseq_srs_meta, fsrs_meta: next_fsrs_meta }
 }
 
+// TODO: supply only card_ref and fsrs_meta
 pub fn review_card(
     cm: &CardMetadata,
     reviewed_at: DateTime<FixedOffset>,
     output_settings: &OutputSettings,
 ) -> Result<()> {
+    // We construct ReviewableFSRSMeta early so as to not require user action
+    // if card is unreviewable.
+    let reviewable_fsrs_meta = ReviewableFSRSMeta::new(&cm.srs_meta.fsrs_meta, reviewed_at)?;
+
     let card = extract_card_by_ref(&cm.card_ref).with_context(|| {
         format!(
             "When extracting card with fingerprint {} from {}, card with prompt prefix: {}",
@@ -94,8 +111,7 @@ pub fn review_card(
     show_card(&card, output_settings)?;
 
     let review_response = wait_for_review()?;
-    let next_srs_meta =
-        compute_next_srs_meta(&card.metadata.srs_meta, &review_response, reviewed_at);
+    let next_srs_meta = compute_next_srs_meta(&reviewable_fsrs_meta, &review_response);
 
     rewrite_card_srs_meta(&card.metadata.card_ref, &next_srs_meta)?;
 
